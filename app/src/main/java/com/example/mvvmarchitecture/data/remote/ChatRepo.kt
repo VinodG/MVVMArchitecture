bc5902dd5f28dd2fcf9a1814e0c9f3b483b30300
SayyadFiroz <fsayyad@unitity.com>
2022-03-16 23:19:01 +0530

    Chat application

--- app/src/main/java/com/example/mvvmarchitecture/data/remote/ChatRepo.kt
+++ app/src/main/java/com/example/mvvmarchitecture/data/remote/ChatRepo.kt
@@ -1,6 +1,7 @@
 package com.example.mvvmarchitecture.data.remote
 
 import android.util.Log
+import io.socket.client.Manager
 import io.socket.client.Socket
 import io.socket.emitter.Emitter
 import javax.inject.Inject
@@ -10,13 +11,16 @@ class ChatRepo @Inject constructor(var socket: Socket?) {
     private val TAG = "ChatRepo"
 
     init {
-        socket?.on(Socket.EVENT_CONNECT, Emitter.Listener {
-            Log.e(TAG, ": OnConnected ")
-        })?.on(Socket.EVENT_DISCONNECT, Emitter.Listener {
-            Log.e(TAG, ": onDisconnected")
-        })?.on(Socket.EVENT_CONNECT_ERROR, Emitter.Listener {
-            Log.e(TAG, ": onError")
-        })
+        socket?.on(Manager.EVENT_TRANSPORT,Emitter.Listener {
+            Log.e(TAG, ": tranport $it")
+
+        })?.on(Socket.EVENT_CONNECT, Emitter.Listener {
+                Log.e(TAG, ": OnConnected ")
+            })?.on(Socket.EVENT_DISCONNECT, Emitter.Listener {
+                Log.e(TAG, ": onDisconnected")
+            })?.on(Socket.EVENT_CONNECT_ERROR, Emitter.Listener {
+                Log.e(TAG, ": onError " + (it[0] as Throwable).message)
+            })
         socket?.connect()
     }
 
